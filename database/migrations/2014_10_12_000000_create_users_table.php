f9896bc5ad512aee1ee0dd4f41e15644ef37e48e
Mihard11 <miharja0501@gmail.com>
2023-10-27 03:51:33 +0700

    first commit

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,13 +12,21 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+        });
+        Schema::table('loans', function (Blueprint $table) {
+            $table->foreignId('user_id')->after('loans_id');
+            $table->foreign('user_id')
+                    ->references('id')
+                    ->on('users')
+                    ->onUpdate('cascade')
+                    ->onDelete('cascade');
         });
     }
 
@@ -27,6 +35,10 @@ return new class extends Migration
      */
     public function down(): void
     {
+        Schema::table('loans', function (Blueprint $table) {
+            $table->dropForeign('loans_user_id_foreign');
+            $table->dropColumn('user_id');
+        });
         Schema::dropIfExists('users');
     }
 };
